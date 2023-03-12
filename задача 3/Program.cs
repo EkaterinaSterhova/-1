13df4ef39e6a5c018735a257bdf05bc436232ca2
EkaterinaSterhova <126870774+EkaterinaSterhova@users.noreply.github.com>
2023-03-12 23:04:33 +0600

    задача3 переделанна и под отрицательные значения

--- задача 3/Program.cs
+++ задача 3/Program.cs
@@ -3,10 +3,10 @@
 // -3 -> нет
 // 7 -> нет
 
-Console.Clear();
+
 Console.Write("Введите число: ");
 int n = Convert.ToInt32(Console.ReadLine());
-if (n % 2 == 1)
+if (n % 2 == -0)
     {Console.WriteLine("yes");}
 else
    { Console.WriteLine("no");}
